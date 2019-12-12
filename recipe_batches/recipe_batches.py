9dfc396fc4fbd11bedc5a8d47fc01836c0919db3
claridiva2000 <claridiva2000@gmail.com>
2019-12-12 01:17:30 -0600

    just glad it works. gotta add error handling

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -6,6 +6,7 @@ def recipe_batches(recipe, ingredients):
   avail= {}
 
   for k,v in recipe.items():
+    print(avail)
     if len(ingredients) < len(recipe):
       return 0
     if v > ingredients[k]:
@@ -13,12 +14,14 @@ def recipe_batches(recipe, ingredients):
     else:
       avail.update({k:ingredients[k]//v})
       print(avail)
-      return min(avail.values())  
+      print((len(avail) < len(recipe) ))
+      return min(avail.values())
+     
 
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
   # your implementation with different inputs
   recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 50, 'flour': 51 }
+  ingredients = { 'milk': 232, 'butter': 150, 'flour': 151 }
   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients)) 
